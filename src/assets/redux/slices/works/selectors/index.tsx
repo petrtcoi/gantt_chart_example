76b022465bdedbbd09c7e393dc9d99f64d56b747
Petr Tcoi <petrtcoi@outlook.com>
2022-11-29 12:28:18 +0300

    fix styles error

--- src/assets/redux/slices/works/selectors/index.tsx
+++ src/assets/redux/slices/works/selectors/index.tsx
@@ -7,16 +7,27 @@ import { RootState } from '../../../store'
 const defEmptyArr = R.defaultTo([])
 const defZero = R.defaultTo(0)
 
+
+// получает rootDate проекта
 export const selectRootDate = (state: RootState) => state.works.rootDay
+// данные о работе (work)
 export const selectWork = (workId: WorkId) => (state: RootState) => state.works.workbyId[workId]
+//  мета-данные работы 
 export const selectMeta = (workId: WorkId) => (state: RootState) => state.works.metaById[workId]
 
 
+/**
+ * Проверяет "upperNodeStatus" у Node, расположенных выше
+ * Если это sibling (prevNode), то просто берем его upperNodeStatus
+ * Есди это родитель (parentNode), 
+ *        (upperNodeStatus OR status) === Collapsed  -> Collapsed
+ *        (в противном случае) -> Expanded
+ * На всякий случай по-умолчанию вернет Collapsed
+ */
 export const selectUpperControlerNodeStatus = (workId: WorkId) => (state: RootState) => {
   const parentNode = state.works.metaById[workId].parentNode
   const prevNode = state.works.metaById[workId].prevNode
   
-
   if (prevNode) return state.works.metaById[prevNode].upperNodeStatus
 
   if (parentNode) {
@@ -53,7 +64,8 @@ export const getChildQnty = (workId: WorkId) => (state: RootState) => {
 
 
 /**
- * getWorkIdList
+ * Получаем список ID работ, развернутый в порядка: сначала дети (child), потом siblings (nextNode)
+ * Возвращаем пустой массив, если почему-то нет rootNodeId
  */
 export const getWorkIdList = (state: RootState) => {
   return (
@@ -64,6 +76,8 @@ export const getWorkIdList = (state: RootState) => {
     )()
   )
 }
+
+
 const getListOfIds = (state: RootState) => getIdsList(state.works.metaById, state.works.rootNodeId)
 function getIdsList(metaById: WorksState['metaById'], workId?: WorkId) {
 
@@ -71,9 +85,12 @@ function getIdsList(metaById: WorksState['metaById'], workId?: WorkId) {
 
   function addWork(workId: WorkId, list?: WorkId[]): WorkId[] {
     const meta = metaById[workId]
+    
     if (!meta) return defEmptyArr(list)
+    
     const nextNode = meta.firstChildNode || meta.nextNode || undefined
     if (!nextNode) return ([...defEmptyArr(list), workId])
+    
     return addWork(nextNode, [...defEmptyArr(list), workId])
   }
 
